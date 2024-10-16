0776bbc1899b2008deaaed6ebbb773acada65d08
CGJI58 <leemsk22@naver.com>
2024-10-17 03:23:46 +0900

    move secret data into the BE .env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { DatabaseService } from './database.service';
 
 @Controller('')
 export class AppController {
+  constructor(private readonly databaseService: DatabaseService) {}
+
   @Get()
-  home() {
-    return { message: 'hello from hi-nest' };
+  database() {
+    return this.databaseService.getDatabaseConfig();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [DatabaseService],
 })
 export class AppModule {}
