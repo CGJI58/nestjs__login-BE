dde018a25bccd9e8d649539d4708fd349c019230
CGJI58 <leemsk22@naver.com>
2024-10-20 01:30:54 +0900

    get access token in BE.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,9 +9,30 @@ export class UsersService {
     return this.users;
   }
 
-  create(code: string) {
+  async create(ghCode: string) {
+    const baseUrl = 'https://github.com/login/oauth/access_token';
+    const config = {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code: ghCode,
+    };
+    const params = new URLSearchParams(config).toString();
+    const accessTokenReqestURL = `${baseUrl}?${params}`;
+
+    const accessTokenReqest = await (
+      await fetch(accessTokenReqestURL, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+    ).json();
+
+    const accessToken = accessTokenReqest.access_token;
+    console.log(accessToken);
     this.users.push({
-      code,
+      ghCode,
+      accessToken,
     });
   }
 }
