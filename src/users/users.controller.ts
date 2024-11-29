b2af43e715817b32ee692141b8bd5c891d777915
CGJI58 <leemsk22@naver.com>
2024-11-30 02:57:08 +0900

    user log in/out done.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UserInfo } from './entities/user.entity';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
@@ -11,8 +11,9 @@ export class UsersController {
     return this.userService.login(ghCode);
   }
 
-  @Get(':ghCode')
-  getUser(@Param('ghCode') ghCode: string): UserInfo {
-    return this.userService.getUser(ghCode);
+  @Patch('logout')
+  logout(@Body() body: any) {
+    const user: User = body.user;
+    return this.userService.logout(user);
   }
 }
