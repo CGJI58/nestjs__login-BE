5dc13923b112faa9fd4f3eb6ff5d8936bbf43b18
CGJI58 <leemsk22@naver.com>
2024-12-13 14:56:50 +0900

    database deploy done(mongodb atlas). db name: oAuthLogInUserDB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
+const ATLAS_USERNAME = 'oAuthTory';
+
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(`mongodb+srv://${ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@oauthloginuserdb.8d3h0.mongodb.net/${process.env.ATLAS_DATABASE_NAME}?retryWrites=true&w=majority
+`),
   ],
   controllers: [AppController],
   providers: [AppService],
