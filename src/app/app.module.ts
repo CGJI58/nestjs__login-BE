a9ac032466eec196ab154599b05cd4098802a194
CGJI58 <leemsk22@naver.com>
2024-12-19 13:03:09 +0900

    add Throttler to protect Too many requests attack
    BE 주소 가리기 위해 reverse proxy 까지 배포 하는건 너무 과한거 같아서 이걸로 대체함

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 const ATLAS_USERNAME = 'oAuthTory';
+const REQ_LIMIT_INTERVAL = 60;
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: REQ_LIMIT_INTERVAL * 1000,
+        limit: 30,
+      },
+    ]),
     UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserEntity } from './entities/user.entity';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('users')
+@UseGuards(ThrottlerGuard)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
